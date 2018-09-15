7f9b0100637cd69c33c067018abc9060c0b2d50f
Lautaro <lautarodelosheros@gmail.com>
2018-09-15 18:26:58 -0300

    Agrego menu deslizante

--- app/src/main/java/taller2_2018_2c_grupo5/comprame/actividades/MainActivity.java
+++ app/src/main/java/taller2_2018_2c_grupo5/comprame/actividades/MainActivity.java
@@ -1,6 +1,11 @@
 package taller2_2018_2c_grupo5.comprame.actividades;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,7 +13,10 @@ import android.view.MenuItem;
 
 import taller2_2018_2c_grupo5.comprame.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout =  findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         FragmentLoader.load(this, BuscarItemsFragment.newInstance("aSessionToken"), "BuscarItems");
 
@@ -41,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
